7d17bd8696e1fd1720c41cd2f5643e344ce7ca15
VladMihalache <59883066+VladMihalache@users.noreply.github.com>
2020-06-17 13:46:41 +0300

    Updates

---
+++ precache-manifest.2ac9b92d7c936bcbed3b4af47dd6dd8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f745bbd4d76fc7a4aa9a558027dc0cfc",
+    "url": "/React-Task-List/index.html"
+  },
+  {
+    "revision": "8a97b4a2f970988fabb6",
+    "url": "/React-Task-List/static/css/main.7aff64e7.chunk.css"
+  },
+  {
+    "revision": "ae829080f9b0c33a538a",
+    "url": "/React-Task-List/static/js/2.d5510226.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Task-List/static/js/2.d5510226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a97b4a2f970988fabb6",
+    "url": "/React-Task-List/static/js/main.97122ecb.chunk.js"
+  },
+  {
+    "revision": "ba9862577f0b2048bc12",
+    "url": "/React-Task-List/static/js/runtime-main.01aab593.js"
+  },
+  {
+    "revision": "5c36d8688dec6400fe1c9a4513f449a8",
+    "url": "/React-Task-List/static/media/avatar.5c36d868.jpg"
+  },
+  {
+    "revision": "f78f595424bbb72b3c2ed37f40499bfe",
+    "url": "/React-Task-List/static/media/bg1.f78f5954.png"
+  }
+]);
